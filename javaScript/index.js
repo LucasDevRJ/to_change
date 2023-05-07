162be3b6616fb2935398d7af912f096bda9b4699
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-05-07 18:07:24 -0300

    Lógica de mudar de cor finalizada

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,7 +1,17 @@
+let cliques = 0;
+
 function trocarCorDoBody() {
 	let corpo = document.querySelector("body");
 
-	corpo.style.backgroundColor = "black";
+	let coresDeFundo = ["#FF0000", "#FFC0CB"];
+	
+	corpo.style.backgroundColor = coresDeFundo[cliques];
+	
+	if (cliques > coresDeFundo.length) {
+		cliques = 0;
+	} else {
+		cliques++;
+	}
 }
 
 let botao = document.querySelector("button");
