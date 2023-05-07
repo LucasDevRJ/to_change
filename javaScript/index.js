596c102641184f9edf5da27bb2e6b00f2c25acd2
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-05-07 18:14:37 -0300

    Adição de mais cores

--- javaScript/index.js
+++ javaScript/index.js
@@ -3,7 +3,8 @@ let cliques = 0;
 function trocarCorDoBody() {
 	let corpo = document.querySelector("body");
 
-	let coresDeFundo = ["#FF0000", "#FFC0CB"];
+	let coresDeFundo = ["#FF0000", "#FFC0CB", "#FF7F50", "#FFA500", "#FFFF00",
+		"#EE82EE", "#800080", "#008000", "#0000FF", "#A52A2A", "#000000"];
 	
 	corpo.style.backgroundColor = coresDeFundo[cliques];
 	
