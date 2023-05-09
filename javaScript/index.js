fc21746a4507dad42ef272f3bd2afffec590095a
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-05-08 21:06:52 -0300

    Responsividade para o mobile

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,18 +1,21 @@
 let cliques = 0;
 
-function trocarCorDoBody() {
-	let corpo = document.querySelector("body");
+let coresDeFundo = ["#98FB98", "#FF0000", "#FFC0CB", "#FF7F50", "#FFA500", "#FFFF00",
+	"#EE82EE", "#800080", "#008000", "#0000FF", "#A52A2A", "#000000", "#F08080",
+	"#FF1493"];
 
-	let coresDeFundo = ["#FF0000", "#FFC0CB", "#FF7F50", "#FFA500", "#FFFF00",
-		"#EE82EE", "#800080", "#008000", "#0000FF", "#A52A2A", "#000000"];
-	
-	corpo.style.backgroundColor = coresDeFundo[cliques];
-	
-	if (cliques > coresDeFundo.length) {
+let corpo = document.querySelector("body");
+
+corpo.style.backgroundColor = coresDeFundo[cliques];
+
+function trocarCorDoBody() {
+	if (cliques >= coresDeFundo.length) {
 		cliques = 0;
-	} else {
+	} else if (cliques < coresDeFundo.length) {
 		cliques++;
 	}
+
+	corpo.style.backgroundColor = coresDeFundo[cliques];
 }
 
 let botao = document.querySelector("button");
