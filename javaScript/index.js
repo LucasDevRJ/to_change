263abb8caf5488a5067b72716305123626682b3e
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-05-08 21:14:59 -0300

    Trocando as cores

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,8 +1,8 @@
 let cliques = 0;
 
-let coresDeFundo = ["#98FB98", "#FF0000", "#FFC0CB", "#FF7F50", "#FFA500", "#FFFF00",
-	"#EE82EE", "#800080", "#008000", "#0000FF", "#A52A2A", "#000000", "#F08080",
-	"#FF1493"];
+let coresDeFundo = ["SpringGreen", "Salmon", "Pink", "OrangeRed", "Purple",
+	"Lime", "Aqua", "Cornsilk", "Chocolate", "Gainsboro", "DarkSlateGray", 
+	"DarkBlue", "OliveDrab", "Indigo", "Lavender"];
 
 let corpo = document.querySelector("body");
 
